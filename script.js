aea400953349db1652a7b63952c36e10aa167a01
ROUSHAN RAJ <roushan.raj656@gmail.com>
2019-08-29 15:44:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function changeFunc() {
+    var selectBox = document.getElementById("selectBox");
+    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    if (selectedValue==="1"){
+            alert("the temperature is 23 degree celsius");
+    }
+    if (selectedValue=="2"){
+
+    }
+    if (selectedValue=="3"){
+
+    }
+    if (selectedValue=="4"){
+
+    }
+   // alert(selectedValue);
+    
+}
